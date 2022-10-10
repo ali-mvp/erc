76c5f1152d3047ac2f4a7eaa9a213001af067d41
alihassan <alihassan.workmail@gmail.com>
2022-10-10 11:29:51 +0400

    Update

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { app, protocol, BrowserWindow } from "electron";
+import { app, protocol, BrowserWindow,dialog } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS3_DEVTOOLS } from "electron-devtools-installer";
 import { autoUpdater } from "electron-updater";
