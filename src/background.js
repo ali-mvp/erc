a24098d7fde4154328613967636fd4e7644a7efa
alihassan <alihassan.workmail@gmail.com>
2022-10-10 17:57:31 +0400

    Update

--- src/background.js
+++ src/background.js
@@ -46,6 +46,9 @@ async function createWindow() {
         autoUpdater.logger = require("electron-log");
         autoUpdater.logger.transports.file.level = "info";
       }
+      if (msg === "install") {
+        autoUpdater.quitAndInstall();
+      }
     });
   }
 }
@@ -97,30 +100,31 @@ if (isDevelopment) {
   }
 }
 
-autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-  log.info("update_available");
-  const dialogOpts = {
-    type: "info",
-    buttons: ["Ok"],
-    title: "Application Update",
-    message: process.platform === "win32" ? releaseNotes : releaseName,
-    detail: "A new version is being downloaded.",
-  };
-  dialog.showMessageBox(dialogOpts, (response) => {});
-});
+// autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+//   log.info("update_available");
+//   const dialogOpts = {
+//     type: "info",
+//     buttons: ["Ok"],
+//     title: "Application Update",
+//     message: process.platform === "win32" ? releaseNotes : releaseName,
+//     detail: "A new version is being downloaded.",
+//   };
+//   dialog.showMessageBox(dialogOpts, (response) => {});
+// });
 
 autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: "info",
-    buttons: ["Restart", "Later"],
-    title: "Application Update",
-    message: process.platform === "win32" ? releaseNotes : releaseName,
-    detail:
-      "A new version has been downloaded. Restart the application to apply the updates.",
-  };
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall();
-  });
+  socket.emit("chat message", input.value);
+  // const dialogOpts = {
+  //   type: "info",
+  //   buttons: ["Restart", "Later"],
+  //   title: "Application Update",
+  //   message: process.platform === "win32" ? releaseNotes : releaseName,
+  //   detail:
+  //     "A new version has been downloaded. Restart the application to apply the updates.",
+  // };
+  // dialog.showMessageBox(dialogOpts).then((returnValue) => {
+  //   if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  // });
 });
 
 autoUpdater.on("update-not-available", () => {
