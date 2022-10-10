cbc6d075cad7239ab23ff191521cdcdfb3801b16
alihassan <alihassan.workmail@gmail.com>
2022-10-10 10:56:35 +0400

    Update

--- src/background.js
+++ src/background.js
@@ -77,8 +77,30 @@ const log = require("electron-log");
 
 autoUpdater.on("update-available", () => {
   log.info("update_available");
-  win.webContents.send("updater", "update_available");
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Ok"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail: "A new version is being downloaded.",
+  };
+  dialog.showMessageBox(dialogOpts, (response) => {});
 });
+
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
+  };
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
+});
+
 autoUpdater.on("update-not-available", () => {
   log.info("update_not_available");
   win.webContents.send("updater", "update_not_available");
