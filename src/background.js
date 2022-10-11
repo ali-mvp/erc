b117f3f2cfe872841100523ac53adf3e12fdf16d
alihassan <alihassan.workmail@gmail.com>
2022-10-11 12:10:12 +0400

    Updater

--- src/background.js
+++ src/background.js
@@ -122,3 +122,16 @@ autoUpdater.on("update-not-available", () => {
   log.info("update_not_available");
   win.webContents.send("updater", "update_not_available");
 });
+
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message =
+    log_message +
+    " (" +
+    progressObj.transferred +
+    "/" +
+    progressObj.total +
+    ")";
+  win.webContents.send("update-progress", log_message);
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="home">
-    <h1>
-      This is test
-    </h1>
+    <h1>This is test</h1>
+    <p>
+      Updating...<br />
+      {{ updateProgress }}
+    </p>
   </div>
 </template>
 
+<script setup lang="js">
+import {onMounted} from "vue"
+
+
+const updateProgress = ref()
+onMounted(()=>{
+  window.ipcRenderer.on("update-progress", (event, message) => {
+
+      updateProgress.value = message
+
+  });
+
+})
+</script>
